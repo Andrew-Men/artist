dde11d32d0bbf2988ef742244be4d884b3e5b2c3
Eis <eis@BALTHASAR.local>
2019-01-28 11:35:10 -0500

    fix bugs

--- artist.py
+++ artist.py
@@ -11,6 +11,7 @@ from keras.preprocessing.image import ImageDataGenerator
 # define parameter
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string("mode", "train", "running mode")
+tf.app.flags.DEFINE_string("model", "cnn", "model")
 tf.app.flags.DEFINE_string("fig", "0", "whether save training visualization figure")
 tf.app.flags.DEFINE_string("save", "0", "whether save the trained model")
 tf.app.flags.DEFINE_float("dropout_rate", 0.2, "")
@@ -77,37 +78,38 @@ def _cnn(filter_num_1, filter_num_2, dropout_rate, learnrate):
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['acc'])
     return model
 def _res(learnrate):
-	inpt = Input(shape=(256,256,3))
-	x = ZeroPadding2D((3,3))(inpt)
-	x = Conv2d_BN(x,nb_filter=64,kernel_size=(7,7),strides=(2,2),padding='valid')
-	x = MaxPooling2D(pool_size=(3,3),strides=(2,2),padding='same')(x)
-	#(56,56,64)
-	x = Conv_Block(x,nb_filter=64,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=64,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=64,kernel_size=(3,3))
-	#(28,28,128)
-	x = Conv_Block(x,nb_filter=128,kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)
-	x = Conv_Block(x,nb_filter=128,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=128,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=128,kernel_size=(3,3))
-	#(14,14,256)
-	x = Conv_Block(x,nb_filter=256,kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)
-	x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
-	#(7,7,512)
-	x = Conv_Block(x,nb_filter=512,kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)
-	x = Conv_Block(x,nb_filter=512,kernel_size=(3,3))
-	x = Conv_Block(x,nb_filter=512,kernel_size=(3,3))
-	x = AveragePooling2D(pool_size=(7,7))(x)
-	x = Flatten()(x)
-	x = Dense(11,activation='softmax')(x)
-	model = Model(inputs=inpt,outputs=x)
-	adam = Adam(lr=learnrate)# 0.0001
+    inpt = Input(shape=(256,256,3))
+    x = ZeroPadding2D((3,3))(inpt)
+    x = Conv2d_BN(x,nb_filter=64,kernel_size=(7,7),strides=(2,2),padding='valid')
+    x = MaxPooling2D(pool_size=(3,3),strides=(2,2),padding='same')(x)
+    #(56,56,64)
+    x = Conv_Block(x,nb_filter=64,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=64,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=64,kernel_size=(3,3))
+    #(28,28,128)
+    x = Conv_Block(x,nb_filter=128,kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)
+    x = Conv_Block(x,nb_filter=128,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=128,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=128,kernel_size=(3,3))
+    #(14,14,256)
+    x = Conv_Block(x,nb_filter=256,kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)
+    x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=256,kernel_size=(3,3))
+    #(7,7,512)
+    x = Conv_Block(x,nb_filter=512,kernel_size=(3,3),strides=(2,2),with_conv_shortcut=True)
+    x = Conv_Block(x,nb_filter=512,kernel_size=(3,3))
+    x = Conv_Block(x,nb_filter=512,kernel_size=(3,3))
+    x = AveragePooling2D(pool_size=(7,7))(x)
+    x = Flatten()(x)
+    x = Dense(11,activation='softmax')(x)
+    model = Model(inputs=inpt,outputs=x)
+    adam = Adam(lr=learnrate)# 0.0001
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['acc'])
-	return model
+    return model
+
 # plot training process
 def _visualize():
     # FIX CRASH #
@@ -131,7 +133,13 @@ def _savemodel():
 
 x_train, y_train = load_and_preprocess()
 if FLAGS.mode == 'train':
-    model = _cnn(filter_num_1, filter_num_2, dropout_rate, learnrate)
+    if FLAGS == "cnn":
+        model = _cnn(filter_num_1, filter_num_2, dropout_rate, learnrate)
+    elif FLAGS == "res":
+        model = _res(learnrate)
+    else:
+        print("wrong parameter: model!")
+        exit()
     history = model.fit(x=x_train, y=y_train, batch_size=32, validation_split=0.2, epochs=50)
     _savemodel()
 elif FLAGS.mode == 'load':
