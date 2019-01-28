bdaa0a803a333995f29a788fe92f22d1a55bba6f
Zachzhang97 <903244773@qq.com>
2019-01-29 00:37:05 +0800

    add res tool

--- artist.py
+++ artist.py
@@ -77,6 +77,29 @@ def _cnn(filter_num_1, filter_num_2, dropout_rate, learnrate):
 
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['acc'])
     return model
+	
+def Conv2d_BN(x, nb_filter,kernel_size, strides=(1,1), padding='same',name=None):
+    if name is not None:
+        bn_name = name + '_bn'
+        conv_name = name + '_conv'
+    else:
+        bn_name = None
+        conv_name = None
+ 
+    x = Conv2D(nb_filter,kernel_size,padding=padding,strides=strides,activation='relu',name=conv_name)(x)
+    x = BatchNormalization(axis=3,name=bn_name)(x)
+    return x
+ 
+def Conv_Block(inpt,nb_filter,kernel_size,strides=(1,1), with_conv_shortcut=False):
+    x = Conv2d_BN(inpt,nb_filter=nb_filter,kernel_size=kernel_size,strides=strides,padding='same')
+    x = Conv2d_BN(x, nb_filter=nb_filter, kernel_size=kernel_size,padding='same')
+    if with_conv_shortcut:
+        shortcut = Conv2d_BN(inpt,nb_filter=nb_filter,strides=strides,kernel_size=kernel_size)
+        x = add([x,shortcut])
+        return x
+    else:
+        x = add([x,inpt])
+        return x
 def _res(learnrate):
     inpt = Input(shape=(256,256,3))
     x = ZeroPadding2D((3,3))(inpt)
