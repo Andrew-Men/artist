450f06a77e422084781889bea33e196d1a3a5a6c
Zachzhang97 <903244773@qq.com>
2019-01-29 14:28:41 +0800

    add .npy

--- predict.py
+++ predict.py
@@ -4,20 +4,24 @@ Created on Tue Jan 29 05:18:28 2019
 
 @author: 90324
 """
-import keras
-from keras.models import load_model
+from tensorflow import keras
 import numpy as np
 train_data_path = 'data/train_input.npy'
 train_label_path = 'data/train_label.npy'
 test_data_path = 'data/test_input.npy'
 
 model_name = 'cnn_model.h5'
-model = load_model(model_name)
+model = keras.load_model(model_name)
 predict_data = np.load(test_data_path)
+model.predict()
 predict_data = predict_data/255.0
 result = model.predict(predict_data)
 result_label = np.argmax(result,axis=1)
 #result_label = [class_names[i] for i in result_label]
-with open('result.txt',"w") as f:
-        f.write(str(result_label)) 
-print('finish predict')
+#with open('result.txt',"w") as f:
+       # f.write(str(result_label)) 
+        
+f = open('result.npy','wb')
+np.save(f,str(result_label))
+f.close()
+print('result finished')
